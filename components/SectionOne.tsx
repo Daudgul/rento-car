7beb53b6bb97bbb93223b938e4952eb8ca91fdb6
Daud gul <codrdaud@gmail.com>
2022-07-02 17:18:53 +0530

    first commit

---
+++ components/SectionOne.tsx
@@ -0,0 +1,12 @@
+import { Box, Container } from "@mui/material";
+import React from "react";
+
+const SectionOne = () => {
+  return (
+    <Container>
+      <Box></Box>
+    </Container>
+  );
+};
+
+export default SectionOne;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,29 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { createTheme, ThemeProvider } from "@mui/material";
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#f06a21",
+    },
+    secondary: {
+      main: "#ff9a2e",
+    },
+    error: {
+      main: "#fbf2e9",
+    },
+    text: {
+      secondary: "#737272",
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
