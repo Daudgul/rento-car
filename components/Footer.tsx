073edad21d0ee67e49dcbe3f9a25b526834c892c
Daud gul <codrdaud@gmail.com>
2022-07-24 16:54:31 +0530

    added some classes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,16 +1,32 @@
 import React from "react";
-import Style from './Footer.module.css'
+import Style from "./Footer.module.css";
 
 const Footer = () => {
-  return <div className={Style.footer}>
-    <ul>
+  return (
+    <div className={Style.footer}>
+      <ul>
         <li>about</li>
         <li>contact us</li>
         <li>info</li>
         <li>projects</li>
         <li>review</li>
-    </ul>
-  </div>;
+      </ul>
+      <ul>
+        <li>about</li>
+        <li>contact us</li>
+        <li>info</li>
+        <li>projects</li>
+        <li>review</li>
+      </ul>
+      <ul>
+        <li>about</li>
+        <li>contact us</li>
+        <li>info</li>
+        <li>projects</li>
+        <li>review</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Footer;
